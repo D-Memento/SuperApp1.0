10a209c71c9bf6e5192556ee6e65b2934dfee6ab
D-Memento <150512756+D-Memento@users.noreply.github.com>
2026-03-15 16:24:26 +0400

    Update ReportGenerator.java

--- System/src/main/java/filesystem/ReportGenerator.java
+++ System/src/main/java/filesystem/ReportGenerator.java
@@ -9,25 +9,25 @@ public class ReportGenerator {
     private List<ProcessTracker.ProcessInfo> processes;
     private FileManager fileManager;
 
-    // Конструктор принимает список процессов и экземпляр FileManager
+    
     public ReportGenerator(List<ProcessTracker.ProcessInfo> processes, FileManager fileManager) {
         this.processes = processes;
         this.fileManager = fileManager;
     }
 
     public void saveReport(String fileName) {
-        // Получаем путь к папке "Мои документы"
+        
         String documentsPath = new File("Мои документы").getAbsolutePath();
         String fullPath = documentsPath + File.separator + fileName;
 
         try (FileWriter writer = new FileWriter(fullPath)) {
-            // Записываем информацию о процессах в файл
+            
             for (ProcessTracker.ProcessInfo process : processes) {
                 writer.write(process.toString() + System.lineSeparator());
             }
             System.out.println("Отчет успешно сохранен в файл: " + fullPath);
 
-            // Добавляем файл в дерево файлового менеджера
+            
             if (fileManager != null) {
                 fileManager.addFileToTree(new File(fullPath));
             }
